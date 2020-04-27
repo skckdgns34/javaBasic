9b900360f1437746b8482eb3613deeff8779f80a
choich <skckdgns34@naver.com>
2020-04-27 09:40:38 +0900

    broe

---
+++ BitReverseOperatorExample.java
@@ -0,0 +1,28 @@
+
+public class BitReverseOperatorExample{
+	
+	public static void main(String[] args){
+		int num1 = 10;
+		int num2 = ~num1;  //   1' complement
+		int num3 = num2 + 1; // 2' complement
+		
+		String returnValue = toBinaryString(num1);
+		System.out.println(returnValue);
+		
+		 returnValue = toBinaryString(num2);
+		System.out.println(returnValue);
+		
+		 returnValue = toBinaryString(num3);
+		System.out.println(returnValue);
+	}
+	
+	public static String toBinaryString(int val){
+		String str = Integer.toBinaryString(val);
+		while(str.length() < 32) {
+			str = "0" + str;
+		}
+		return str;
+		
+	}
+	
+}
